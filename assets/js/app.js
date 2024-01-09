7f96be2541b5f0f9fbefff41f1e0ca8b356672a6
Farideh <heidarian89@gmail.com>
2024-01-08 18:44:18 -0800

    add typing animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,31 +12,31 @@ function showTime() {
 }
 
 // DATE FORM-1
-function showDate() {
-  const date = new Date();
-  const options = {
-    weekday: "long",
-    month: "long",
-    day: "numeric",
-    year: "numeric",
-  };
-  const formattedDate = date.toLocaleDateString("en-US", options);
-
-  const dateElement = document.getElementById("date");
-  dateElement.innerText = formattedDate;
-}
-
-// DATE FORM-2
 // function showDate() {
 //   const date = new Date();
-//   const year = addZero(date.getFullYear());
-//   const month = addZero(date.getMonth() + 1);
-//   const day = addZero(date.getDate());
-//   const formattedDate = `${month}:${day}:${year}`;
+//   const options = {
+//     weekday: "long",
+//     month: "long",
+//     day: "numeric",
+//     year: "numeric",
+//   };
+//   const formattedDate = date.toLocaleDateString("en-US", options);
 
 //   const dateElement = document.getElementById("date");
 //   dateElement.innerText = formattedDate;
 // }
+
+// DATE FORM-2
+function showDate() {
+  const date = new Date();
+  const year = addZero(date.getFullYear());
+  const month = addZero(date.getMonth() + 1);
+  const day = addZero(date.getDate());
+  const formattedDate = `${month}:${day}:${year}`;
+
+  const dateElement = document.getElementById("date");
+  dateElement.innerText = formattedDate;
+}
 
 setInterval(showDate, 1000);
 setInterval(showTime, 1000);
